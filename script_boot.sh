650d7629944b022c8fe9f6b5e3fea200384cf18b
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-04-08 21:42:43 +0000

    change

--- script_boot.sh
+++ script_boot.sh
@@ -6,3 +6,13 @@ ls -lai
 #shows the inodo
 ls -l -a -i
 # the same options but in a separate way
+stat .
+
+# change directory  to home directory from any location of operative
+cd 
+
+# change directory to home directory using obsoluty route
+cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026
+
+# change directory to home dorectory using ~ shortcut
+cd ~
