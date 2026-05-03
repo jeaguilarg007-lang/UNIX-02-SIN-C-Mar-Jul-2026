35f13ca5e3d7bdf02f9839b8a8f8725967c071a4
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-05-03 21:48:36 +0000

    change

---
+++ script_linux_cisco_1.sh
@@ -0,0 +1,19 @@
+ls
+#The ls command is a basic instruction in the command-line interface (CLI) used to display a list of files and directories in a system folder.
+#When executed without options or arguments, it shows the contents of the current directory by default, allowing the user to quickly see what files are available in that location.
+![ls](image.png) 
+
+ls Documents 
+#It is the use of the ls command with an argument (Documents) to display the list of files and directories inside a specific folder, instead of the current directory.
+![ls Documents](image_copy.png)
+
+aptitude moo
+#It is a special or hidden argument (an Easter egg) used with the aptitude command that produces a humorous or non-functional message, rather than performing a standard system task.
+![aptitude moo](image_copy2.png)
+
+ls -l
+#
+![ls -l](image_copy3.png)
+
+ls -r
+#
