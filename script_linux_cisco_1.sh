7b8d6d3ead817623414147c149d6b0a69b595914
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-05-03 22:17:31 +0000

    change

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -12,8 +12,41 @@ aptitude moo
 ![aptitude moo](image_copy2.png)
 
 ls -l
-#
+#Displays a detailed (long format) list of files and directories, including permissions, owner, size, and last modification date.
 ![ls -l](image_copy3.png)
 
 ls -r
-#
+#Displays the list of files and directories in reverse alphabetical order.
+![ls -r](image_copy4.png)
+
+ls -l -r 
+#Displays a detailed list of files in reverse alphabetical order by combining long format and reverse options.
+![ls -l -r](image_copy5.png)
+
+ls -rl
+#Displays a detailed list of files in reverse alphabetical order. This is the same as using `ls -l -r`.
+![ls -rl](image_copy6.png)
+
+ls -lr
+#Displays a detailed list of files in reverse alphabetical order. This is another way to combine `-l` and `-r`.
+![ls -lr](image_copy7.png)
+
+aptitude moo
+#A special Easter egg command that shows a humorous message instead of performing a normal task.
+![aptitude moo](image_copy8.png)
+
+aptitude -v moo
+#Increases verbosity and displays a slightly different humorous message.
+![aptitude -v moo](image_copy9.png)
+
+aptitude -v -v moo 
+#Uses the verbose option twice to further modify the message output.
+![aptitude -v -v moo](image_copy10.png)
+
+aptitude -vv moo
+#Equivalent to `-v -v`, increases verbosity and changes the message again.
+![aptitude -vv moo](image_copy12.png)
+
+aptitude -vvv moo
+#Applies maximum verbosity, resulting in a short and more expressive humorous response.
+![aptitude -vvv moo](image_copy13.png)
