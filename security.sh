33c3a16481a770ada56a27940a8b1ab53690a1fa
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-05-08 15:00:45 +0000

    change

--- security.sh
+++ security.sh
@@ -64,4 +64,53 @@ drwxrwxrwx+ 7 root root  4096 may  8 14:40 .git
 -rw-rw-rw-  1 root root  1517 may  8 14:38 security.sh
 -rw-rw-rw-  1 root root     0 may  8 14:40 text.txt
 
+cat /etc/group   
+#Displays the contents of the /etc/group file, which contains group account information.       
+root:x:0:
+daemon:x:1:
+bin:x:2:
+sys:x:3:
+adm:x:4:
+tty:x:5:
+disk:x:6:
+lp:x:7:
+mail:x:8:
+news:x:9:
+uucp:x:10:
+man:x:12:
+proxy:x:13:
+kmem:x:15:
+dialout:x:20:
+fax:x:21:
+voice:x:22:
+cdrom:x:24:
+floppy:x:25:
+tape:x:26:
+sudo:x:27:
+audio:x:29:
+dip:x:30:
+www-data:x:33:
+backup:x:34:
+operator:x:37:
+list:x:38:
+irc:x:39:
+src:x:40:
+shadow:x:42:
+utmp:x:43:
+video:x:44:
+sasl:x:45:
+plugdev:x:46:
+staff:x:50:
+games:x:60:
+users:x:100:
+nogroup:x:65534:
+lpadmin:x:999:
+systemd-journal:x:998:
+systemd-network:x:997:
+debian-tor:x:101:
+tcpdump:x:996:
+
+groups $USER
+#Displays the groups that the current user belongs to, using the $USER environment variable to specify the user.
+root : root
 
