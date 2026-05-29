ba9472f961d58d5c6e9c75b6851b5ed29f63f19c
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-05-29 15:08:28 +0000

    change

--- security.sh
+++ security.sh
@@ -1,6 +1,5 @@
 touch security.sh
 #
-
 id
 #
 ##uid=0(root) gid=0(root) grupos=0(root) 
@@ -16,3 +15,12 @@ touch ~/test_grupo_heredado.txt
 ls -la ~/test_grupo_heredado.txt
 #
 ##-rw-r--r-- 1 root root 0 may 29 14:57 /root/test_grupo_heredado.txt
+
+id -gn
+#
+##root
+touch ~/antes_de_newgrp.txt
+#
+ls -la ~/antes_de_newgrp.txt
+#
+##-rw-r--r-- 1 root root 0 may 29 15:04 /root/antes_de_newgrp.txt
