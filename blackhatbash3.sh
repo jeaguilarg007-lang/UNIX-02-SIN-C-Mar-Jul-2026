b739b02f7c7188bd5503d5033ba4b9c1b96e0e60
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-06-01 15:42:24 +0000

    change

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -5,3 +5,13 @@ book="black hat bash"
 echo "This book's name is $book"
 
 echo "This book's name is $book"
+
+root_directory=$(ls -ld /)
+
+echo "${root_directory}"
+
+book="Black Hat Bash"
+
+unset book
+
+echo "${book}"
