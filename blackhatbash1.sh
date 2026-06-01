fa1007058113704f67854cac6af7171b4511eb6a
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-06-01 15:21:04 +0000

    change

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,4 +1,8 @@
-bash --version
+#!/bin/bash
+set -x
+## Enables debug mode in the shell
+# (set -x makes Bash print each command and its arguments before executing it, useful for tracing scripts)
+#bash --version
 
 env
 
@@ -17,10 +21,27 @@ ps -ef
 df --human-readable
 # Displays disk usage of the system in a human-readable format (KB, MB, GB)
 
-bash -x blackhatbash1.sh
+#bash -x blackhatbash1.sh
 # Runs the script "blackhatbash1.sh" showing each command as it is executed
 # (-x enables debug mode to trace the script step by step)
 
-bash -r blackhatbash1.sh
+#bash -r blackhatbash1.sh
 # Runs the script "blackhatbash1.sh" in restricted mode
 # (-r limits several shell features for security, blocking potentially dangerous actions)
+
+chmod u+x blackhatbash1.sh
+# Gives the owner (user) permission to execute the file "blackhatbash1.sh"
+
+#./blackhatbash1.sh
+# Runs the script "blackhatbash1.sh" from the current directory
+# (./ means execute a file located in the current folder)
+
+#bash -n blackhatbash1.sh
+
+set +x
+## Disables debug mode in the shell
+# (set +x turns off command tracing so commands are no longer printed before execution)
+
+# bash blackhatbash1.sh
+# Executes the script "blackhatbash1.sh" using the Bash shell
+# (runs the script without requiring execute permissions if called directly with bash)
