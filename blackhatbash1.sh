14d73d1bbd40872742513ffda2a27a4bdc929828
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-06-01 15:07:52 +0000

    change

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -15,6 +15,12 @@ ps -e -f
 ps -ef
 
 df --human-readable
+# Displays disk usage of the system in a human-readable format (KB, MB, GB)
 
-bash -x script.sh
+bash -x blackhatbash1.sh
+# Runs the script "blackhatbash1.sh" showing each command as it is executed
+# (-x enables debug mode to trace the script step by step)
 
+bash -r blackhatbash1.sh
+# Runs the script "blackhatbash1.sh" in restricted mode
+# (-r limits several shell features for security, blocking potentially dangerous actions)
