34628d171413679f6ccc931f562cb0249b7bf01f
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-06-05 15:30:02 +0000

    change

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -7,3 +7,13 @@ ls; ps; whoami
 
 lzl || echo "the lzl command failed"
 #el comando lzl fallo 
+
+echo "Hello World!" > output.txt
+cat output.txt
+#Hello World!
+
+$ echo "Goodbye!" >> output.txt
+$ cat output.txt
+#Hello World!
+#Goodbye!
+cat output.txt
