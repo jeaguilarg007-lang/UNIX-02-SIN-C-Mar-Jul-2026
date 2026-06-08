5c53cc747b90cefd3a65e38ff7e2a280cf3ba8dd
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-06-08 15:15:24 +0000

    change

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -4,4 +4,12 @@ SCRIPT_NAME="${0}"
 TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
-ping "${TARGET}"
+ping -c 3 "${TARGET}"
+echo "The arguments are: $@"
+echo "The total number of arguments is: $#"
+
+#---------------------------------------------------------
+#bash ping_with_arguments.sh 127.0.0.1
+#I have to put the IP number of localhost.
+#chmod u+x ping_with_arguments.sh
+#./ping_with_arguments.sh nostarch.com
