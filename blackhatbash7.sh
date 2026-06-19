173b9d963e27d8928b022e1cb7bf2623b8b94c9b
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-06-19 15:14:04 +0000

    change

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 touch example_file1 example_file2 example_file3
+
+grep "35.237.4.214" log.txt
+
+grep "35.237.4.214\|13.66.139.0" log.txt
+
+grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+
+ps
+
+ps | grep TTY
+
+ps | grep -i tty
+
+grep -v "35.237.4.214" log.txt
