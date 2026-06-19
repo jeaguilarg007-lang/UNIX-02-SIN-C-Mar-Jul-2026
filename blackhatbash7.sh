6878e5c215e4563789ec25048a80d9b21699ace6
jeaguilarg007-lang <jeaguilarg007@gmail.com>
2026-06-19 15:50:58 +0000

    change

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -35,3 +35,8 @@ sed 's/Mozilla/Godzilla/g' log.txt
 
 sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
 sed 's/ //g' log.txt
+sed '1d' log.txt
+sed '$d' log.txt
+sed '5,7d' log.txt
+sed -n '2,15 p' log.txt
+sed -i '1d' log.txt
